b60fe7f32da82456f6bfb629361f332abfd7163c
simpletondl <simpletondl@yandex.ru>
2019-12-01 23:35:45 +0300

    Fix pointers

--- substring.cu
+++ substring.cu
@@ -3,16 +3,19 @@
 #define BLOCKS  16
 #define THREADS 64
 
-__device__ int isSubstring;
-
 __global__ 
-void findSubstring(const char *string, const char *sample, int stringLength, int patternLength)
+void findSubstring(
+		const char *string, 
+		const char *sample, 
+		int stringLength, 
+		int patternLength,
+		volatile int *result)
 {
     int packSize =  
         stringLength < BLOCKS*THREADS ? stringLength / (BLOCKS*THREADS) : 1;
     int index = (threadIdx.x + blockIdx.x * blockDim.x) * packSize;
 
-    if (index + packSize >= BLOCKS*THREADS)
+    if (index + packSize > BLOCKS*THREADS)
     {
         packSize = BLOCKS*THREADS - index;
     }
@@ -20,6 +23,8 @@ void findSubstring(const char *string, const char *sample, int stringLength, int
     {
         return;
     }
+
+    //isSubstring = 10;
 
     for (int i = index; i < index + packSize; ++i)
     {   
@@ -39,7 +44,7 @@ void findSubstring(const char *string, const char *sample, int stringLength, int
 
         if (j == patternLength)
         {
-            isSubstring = 1;
+//            isSubstring = 1;
         }
     }
 }
@@ -47,20 +52,24 @@ void findSubstring(const char *string, const char *sample, int stringLength, int
 
 int main()
 {
-    int result = 0;
-    cudaMemcpyToSymbol("isSubstring", &result, sizeof(result), 0, cudaMemcpyHostToDevice);
-
-    char *string, *pattern;
+	int *result;
+    cudaMalloc((void **) &result, sizeof(int));
+	cudaMemset((void *) result, 0, sizeof(int));
+	
+/*    char *string, *pattern;
     cudaMalloc((void **) &string, 11);
     cudaMalloc((void **) &pattern, 5);
 
     cudaMemcpy((void **) &string, (const void *) "abcdefghij\0", 11, cudaMemcpyHostToDevice);
     cudaMemcpy((void **) &pattern, (const void *) "defg\0", 5, cudaMemcpyHostToDevice);
 
-    findSubstring<<<BLOCKS, THREADS>>>(string, pattern, 10, 4);
+    findSubstring<<<BLOCKS, THREADS>>>(string, pattern, 10, 4);*/
 
-    cudaMemcpyFromSymbol(&result, "isSubstring", sizeof(result), cudaMemcpyDeviceToHost);
-    printf("%d", result);
+    int *kek;
+	kek = (int *) malloc(sizeof(int));
+	*kek = 5;
+	cudaMemcpy((void *) kek, (void *) result, sizeof(int), cudaMemcpyDeviceToHost);
+    printf("%d\n", *kek);
 
     return 0;
 }
