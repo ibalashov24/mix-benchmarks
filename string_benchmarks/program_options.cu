8133c8eb26da686649e7bec4107fa43a927f42c5
Ilya Balashov <ibalashov24@gmail.com>
2020-02-25 16:29:06 +0000

    Change ambigious options

--- string_benchmarks/program_options.cu
+++ string_benchmarks/program_options.cu
@@ -9,11 +9,12 @@ po::options_description init_args()
 {
     po::options_description desc("Allowed options");
     desc.add_options()
+	("help,h", "Produce help")
         ("data_file,d", po::value<std::string>()->default_value("data.in"), "Set filename for data")
         ("pattern_file,p", po::value<std::string>()->default_value("pattern.in"), "Set filename for pattern")
 	("pattern_count,c", po::value<int>()->default_value(1), "Set pattern count")
-	("pattern_size,ps", po::value<int>()->default_value(100), "Set single pattern size in bytes")
-	("data_size,ds", po::value<int>()->default_value(1048576), "Set data size in bytes");
+	("pattern_size,a", po::value<int>()->default_value(100), "Set single pattern size in bytes")
+	("data_size,s", po::value<int>()->default_value(1048576), "Set data size in bytes");
 
     return desc;
 }
@@ -29,7 +30,8 @@ Arguments read_arguments(int argc, char** argv)
     po::variables_map vm;
     try
     {
-        po::store(po::parse_command_line(argc, argv, args_desc), vm);
+        po::store(po::command_line_parser(argc, argv).options(args_desc).style(po::command_line_style::unix_style).run(), vm);
+        po::notify(vm);
 
         result.data_file = vm["data_file"].as<std::string>();
         result.pattern_file = vm["pattern_file"].as<std::string>();
@@ -37,7 +39,6 @@ Arguments read_arguments(int argc, char** argv)
 	result.pattern_length = vm["pattern_size"].as<int>();
 	result.pattern_count = vm["pattern_count"].as<int>();
         
-        po::notify(vm);
     }
     catch(po::error &e)
     {
