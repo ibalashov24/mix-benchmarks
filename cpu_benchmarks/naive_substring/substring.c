9e8d15c24af08885bffc23a4554c60b4d5c01aeb
Ilya Balashov <ibalashov24@gmail.com>
2020-04-13 01:49:22 +0300

    Add CPU naive benchmark

---
+++ cpu_benchmarks/naive_substring/substring.c
@@ -0,0 +1,27 @@
+#include "substring.h"
+	
+int find_substring(
+        const struct Char *data, 
+        int data_length, 
+        __stage(1) const struct Char *pattern, 
+        __stage(1) int pattern_length) __stage(1)
+{
+    for (int i = 0; i < data_length; ++i)
+    {
+        int j = 0;
+        for (j = 0; j < pattern_length; ++j)
+            if (i + j >= pattern_length || data[i + j].c != pattern[j].c)
+                break;
+
+        if (j == pattern_length)
+            return i;
+    }
+
+    return -1;
+}
+
+__attribute((mix(find_substring)))
+void *mix_find_substring(
+        void *context,
+        const struct Char *pattern,
+        int pattern_length);
