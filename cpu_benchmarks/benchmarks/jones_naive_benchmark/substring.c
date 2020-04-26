de4583935b2f0ab89696d0672b8a5060e390bf11
Ilya Balashov <ibalashov24@gmail.com>
2020-04-26 16:48:06 +0300

    Adapt benchmark for utilizing negative information

--- cpu_benchmarks/benchmarks/jones_naive_benchmark/substring.c
+++ cpu_benchmarks/benchmarks/jones_naive_benchmark/substring.c
@@ -1,59 +1,54 @@
-//#include "substring.h"
+#include "substring.h"
 
 #include <stdlib.h>
 
-struct Char
-{
-	char c;
-};
-
 __stage(1)
-int find_substring(__stage(1) const struct Char *data, 
-		   __stage(1) int data_size,
+long long find_substring(__stage(1) const struct Char *data, 
+		   __stage(1) long long data_size,
 		   const struct Char *pattern, 
-		   int pattern_size) __stage(1)
+		   long long pattern_size) __stage(1)
 {
-	struct Char stack[250];
+	struct Char stack[500];
+	//struct Char *stack = malloc(pattern_size + 10);
 
-	int result = -1;
-	int st_pointer = 0;
-	int cache_pointer = st_pointer;
-	for (int i = 0; i < data_size; ++i)
+	long long result = -1;
+	long long st_pointer  = 0;
+	long long cache_pointer = st_pointer ;
+	for (long long i = 0; i < data_size; ++i)
 	{
-		int flag = 1;
-		int j = 0;
-		for (; j < pattern_size; )
+		long long j = 0;
+		while (j < pattern_size && j != -1)
 		{
-			if (st_pointer == 0)
+			if (st_pointer  == 0)
 			{
 				// Static pattern stack is empty and we have an equality
-				if (data[i].c == pattern[i + j].c)					
+				if (data[i + j].c == pattern[j].c)					
 				{
-				  	stack[cache_pointer++].c = pattern[j].c;
-					st_pointer = 0;
+				  	stack[cache_pointer ++].c = pattern[j].c;
+					st_pointer  = 0;
 					++j;
 				}
 				// Static pattern cache is empty . using dynamic pattern
-				else if (cache_pointer == 0)
-					flag = -1; // Leaving loop
+				else if (cache_pointer  == 0)
+					j = -1; // Leaving loop
 				// Static pattern cache is not empty . we should use it
 				else
 				{
-					--cache_pointer;
-					st_pointer = cache_pointer;
+					--cache_pointer ;
+					st_pointer  = cache_pointer ;
 				}
 			} 
 			else
 			// Static pattern stack is not empty . using it
 			{
-				if (pattern[i + j].c == stack[st_pointer].c)
+				if (pattern[j].c == stack[st_pointer - 1].c)
 				{
-					--st_pointer;
+					--st_pointer ;
 				}
 				else
 				{
-					--cache_pointer;
-					st_pointer = cache_pointer;
+					--cache_pointer ;
+					st_pointer  = cache_pointer ;
 				}
 			}
 		}
@@ -62,8 +57,10 @@ int find_substring(__stage(1) const struct Char *data,
 			result = j;
 	}	
 
+	free(stack);
+
 	return result;
 }
 
 __attribute__((mix(find_substring)))
-void *mix_find_substring(void *context, const struct Char *data, int data_size);
+void *mix_find_substring(void *context, const struct Char *data, long long data_size);
--- cpu_benchmarks/benchmarks/naive_substring/launcher.cpp
+++ cpu_benchmarks/benchmarks/naive_substring/launcher.cpp
@@ -1,16 +1,25 @@
 #include <random>
 #include <cstdint>
 #include <algorithm>
+#include <fstream>
 
 #include "../Compiler.h"
 #include "substring.h"
 
 #include "benchmark/benchmark.h"
 
+//const long long StartSize = 419430400;        // 400mb
+//const long long DataSourceSize = 10737418240; // 10gb
+//const long long MaxDataSize = 3355443200;     // 3200mb 
+//const long long DataSizeStep = 419430400;     // 400mb
+//const long long PatternLength = 200;          // 200 bytes
+
+const long long StartSize = 4194304;        // 400mb
 const long long DataSourceSize = 10737418240; // 10gb
-const long long MaxDataSize = 3355443200;     // 3200mb 
-const long long DataSizeStep = 209715200;     // 200mb
-const long long PatternLength = 200;          // 200 bytes
+const long long MaxDataSize = 33554432;     // 3200mb 
+const long long DataSizeStep = 4194304;     // 400mb
+const long long PatternLength = 20;          // 200 bytes
+
 
 char *generate_data(long long data_size)
 {
@@ -34,22 +43,33 @@ void BM_naive_substring(benchmark::State &state)
     std::mt19937 gen(rd());
     std::uniform_real_distribution<> dis(0, DataSourceSize - state.range(0) - 1);
 
+//    std::ofstream err("kek.out", std::ios::app);
+
+
     for (auto _: state) 
     {
-        benchmark::DoNotOptimize(
-                find_substring(
+        
+        int result =        find_substring(
                     data_source + (long long) (dis(gen)), 
                     state.range(0), 
                     pattern, 
-                    PatternLength));
+                    PatternLength);
+
+//	err << result << std::endl;
     }
+
+//    err.close();
+
+    delete[] pattern;
+    delete[] data_source;
 }                       
-BENCHMARK(BM_naive_substring)->DenseRange(200, MaxDataSize, DataSizeStep);
+BENCHMARK(BM_naive_substring)->DenseRange(StartSize, MaxDataSize, DataSizeStep);
+//BENCHMARK(BM_naive_substring)->DenseRange(419430400, 419430405, 1);
 
 void BM_naive_subtring_mix(benchmark::State &state)
 {
-    auto pattern = (Char *) generate_data(PatternLength);
-    auto data_source = (Char *) generate_data(DataSourceSize);
+    auto pattern = reinterpret_cast<Char *>(generate_data(PatternLength));
+    auto data_source = reinterpret_cast<Char *>(generate_data(DataSourceSize));
  
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -62,12 +82,24 @@ void BM_naive_subtring_mix(benchmark::State &state)
                 &compiler.getContext(), 
                 pattern, 
                 PatternLength));
-    auto *spec = reinterpret_cast<int (*)(const Char *, int)>(compiler.compile());
+    auto *spec = reinterpret_cast<long long (*)(const Char *, long long)>(compiler.compile());
+
+//    std::ios::sync_with_stdio(false);
+//    std::ofstream err("kek.out");
 
     for (auto _: state)
     {
-        int result = spec(data_source + (long long) (dis(gen)), state.range(0));
+	auto current_data = data_source + (long long) (dis(gen));
+        auto result = spec(reinterpret_cast<const Char *>(current_data), state.range(0));
+
+//	err << result << std::endl;
     }
+
+//    err.close();
+    
+
+    delete[] pattern;
+    delete[] data_source;
 }
-BENCHMARK(BM_naive_subtring_mix)->DenseRange(200, MaxDataSize, DataSizeStep);
+//BENCHMARK(BM_naive_subtring_mix)->DenseRange(StartSize, MaxDataSize, DataSizeStep);
 
